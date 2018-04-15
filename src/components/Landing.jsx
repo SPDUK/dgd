e08790fdcc26b832430e223ee5cae4b3a5c81885
spdev <spdevuk@gmail.com>
2018-04-15 11:51:09 +0100

    added logo and date info for landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,12 +3,20 @@ import { Link } from 'react-router-dom';
 
 import '../styles/landing.css';
 import landingImage from '../images/background.jpg';
+import dgdTitle from '../images/dgdtitle.png';
 
 class Landing extends Component {
   render() {
     return (
       <div className="landing">
         <div className="landing-background" />
+        <div className="landing-title">
+          <img src={dgdTitle} />
+        </div>
+        <div className="landing-info">
+          <h2 className="landing-info-album">ARTIFICIAL SELECTION</h2>
+          <h2 className="landing-info-date">JUNE 8TH 2018</h2>
+        </div>
       </div>
     );
   }
