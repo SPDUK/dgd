55b40def9ef26b2c1772deecc8ec51d9a091ddca
spdev <spdevuk@gmail.com>
2018-04-15 11:05:36 +0100

    added responsive background page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../styles/landing.css';
+import landingImage from '../images/background.jpg';
 
 class Landing extends Component {
   render() {
     return (
-      <div className="HelloWorld">
-        <p>Create React App Landing Page!</p>
-        <Link to="/helloworld" className="button">
-          <button>Hello World!</button>
-        </Link>
+      <div className="landing">
+        <div className="landing-background" />
       </div>
     );
   }
