df9173e81e7938eab01d34586ac0d4a361a1e496
spdev <spdevuk@gmail.com>
2018-04-20 21:11:50 +0100

    moved things globally but only called when component mounts

--- src/components/landing/nexttour/NextTour.jsx
+++ src/components/landing/nexttour/NextTour.jsx
@@ -6,55 +6,56 @@ import './nexttour.css';
 
 import nexttourimg from '../../../images/nexttour.jpg';
 
+function getDate() {
+  // find each tour date piece
+  const tourDate = document.querySelector('.nexttour-date-day');
+  const tourCity = document.querySelector('.nexttour-date-city');
+  const tourCountry = document.querySelector('.nexttour-date-country');
+  // const tourClickURL = ' ';
+  // find todays date in a way to split it up and put into the url
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  const yyyy = today.getFullYear();
+  if (dd < 10) {
+    dd = `0${dd}`;
+  }
+  if (mm < 10) {
+    mm = `0${mm}`;
+  }
+  today = `${yyyy}-${mm}-${dd}`;
+  const endDate = `${yyyy + 1}-${mm}-${dd}`;
+  // needs a date to start and end, 1 year from the current date of the request
+  const nextTourURL = `https://rest.bandsintown.com/artists/Dance%20Gavin%20Dance/events?app_id=dgdsite&date=${today}%2C${endDate}`;
+
+  axios.get(nextTourURL).then(res => {
+    // create a new date then turn it to a readable string and cut off the
+    // start and end to show just the month and date
+    const tourDateString = new Date(res.data[0].datetime)
+      .toDateString()
+      .slice(3, -4);
+    console.log(res);
+    tourDate.innerText = tourDateString;
+    tourCity.innerText = res.data[0].venue.city;
+    tourCountry.innerText = res.data[0].venue.country;
+  });
+}
+
 class NextTour extends Component {
   componentDidMount() {
-    // find each tour date piece
-    const tourDate = document.querySelector('.nexttour-date-day');
-    console.log(tourDate);
-    const tourCity = document.querySelector('.nexttour-date-city');
-    console.log(tourCity);
-    const tourCountry = document.querySelector('.nexttour-date-country');
-    console.log(tourCountry);
-
-    // find todays date in a way to split it up and put into the url
-    let today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth() + 1;
-    const yyyy = today.getFullYear();
-    if (dd < 10) {
-      dd = `0${dd}`;
-    }
-    if (mm < 10) {
-      mm = `0${mm}`;
-    }
-    today = `${yyyy}-${mm}-${dd}`;
-    const endDate = `${yyyy + 1}-${mm}-${dd}`;
-    // needs a date to start and end, 1 year from the current date of the request
-    const nextTourURL = `https://rest.bandsintown.com/artists/Dance%20Gavin%20Dance/events?app_id=dgdsite&date=${today}%2C${endDate}`;
-    axios.get(nextTourURL).then(res => {
-      console.log(res);
-      const tourDateString = new Date(res.data[0].datetime)
-        .toDateString()
-        .slice(3, -4);
-      console.log(tourDateString);
-      tourDate.innerText = tourDateString;
-      tourCity.innerText = res.data[0].venue.city;
-      tourCountry.innerText = res.data[0].venue.country;
-    });
+    getDate();
   }
+
   render() {
-    function haha() {
-      console.log('lmnaop');
-    }
     return (
-      <div onClick={haha} className="nexttour">
+      <div className="nexttour">
         <div className="nexttour-title">
           <h1>NEXT LIVE SHOW</h1>
         </div>
         <div className="nexttour-date">
-          <h2 className="nexttour-date-day" />
-          <h2 className="nexttour-date-city" />
-          <h2 className="nexttour-date-country" />
+          <h2 className="nexttour-date-day">Jan 1</h2>
+          <h2 className="nexttour-date-city">London</h2>
+          <h2 className="nexttour-date-country">England</h2>
         </div>
         <div className="nexttour-image">
           <img src={nexttourimg} alt="" />
