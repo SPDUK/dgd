114b48cb3ba0c7fa35b246d7dbb52deec197acf3
spdev <spdevuk@gmail.com>
2018-04-21 22:13:29 +0100

    added functions and links with styled focus border

--- src/components/landing/sociallinks/sociallinks.jsx
+++ src/components/landing/sociallinks/sociallinks.jsx
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
 
 import './socialinks.css';
-
+// keeps the website open while linking to social media
+function facebookLink() {
+  window.open('https://www.facebook.com/DanceGavinDance/');
+}
+function twitterLink() {
+  window.open('https://twitter.com/DGDtheband');
+}
+function instagramLink() {
+  window.open('https://www.instagram.com/dancegavindance/');
+}
+function youtubeLink() {
+  window.open(
+    'https://www.youtube.com/watch?v=Vl2Rmw5hsao&list=PLV8pJK07hP1gSr0XG1-0iE5VR1zsjKtCL&index=1'
+  );
+}
 class SocialLinks extends Component {
   render() {
     return (
       <div className="sociallinks">
-        <p>ree</p>
+        <div className="sociallinks-bar">
+          <i
+            role="link"
+            onKeyPress={facebookLink}
+            onClick={facebookLink}
+            tabIndex="0"
+            className="fa fa-facebook"
+          />
+          <i
+            role="link"
+            onKeyPress={twitterLink}
+            onClick={twitterLink}
+            tabIndex="0"
+            className="fa fa-twitter"
+          />
+          <i
+            role="link"
+            onKeyPress={instagramLink}
+            onClick={instagramLink}
+            tabIndex="0"
+            className="fa fa-instagram"
+          />
+          <i
+            role="link"
+            onKeyPress={youtubeLink}
+            onClick={youtubeLink}
+            tabIndex="0"
+            className="fa fa-youtube"
+          />
+        </div>
       </div>
     );
   }
