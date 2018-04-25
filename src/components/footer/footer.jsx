10fe0081a77f60b5078f3984c5a624ee25b36993
spdev <spdevuk@gmail.com>
2018-04-25 17:54:02 +0100

    added footer file

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
+
+import './footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <p>hello footer</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
