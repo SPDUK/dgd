74a2a0ba1e32beadd12fe0a5634f2f03f8c6f71a
spdev <spdevuk@gmail.com>
2018-04-26 22:56:31 +0100

    added footer icons and logo

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -8,7 +8,32 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer">
+        <div className="footer-top">
+          <div className="footer-top-icons">
+              <i className="fab fa-facebook-f" />
+            <i className="fab fa-instagram" />
+            <i className="fab fa-twitter" />
+            <i className="fab fa-youtube" />
+          </div>
+          <div className="footer-top-logo">
+            <img
+              src="http://res.cloudinary.com/dmjolhdaq/image/upload/v1524694763/dgdwhite.png"
+              alt=""
+            />
+          </div>
+          <div className="footer-top-icons">
+            <i className="fab fa-spotify" />
+            <i className="fab fa-soundcloud" />
+            <i className="fab fa-google-play" />
+            <i className="fab fa-itunes-note" />
+          </div>
+        </div>
+        <div className="footer-middle">
           <p>hi</p>
+        </div>
+        <div className="footer-bottom">
+          <p>hi</p>
+        </div>
       </div>
     );
   }
