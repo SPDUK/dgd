52800131e7b290ef7b519527d70092b20764261c
spdev <spdevuk@gmail.com>
2018-04-29 03:01:49 +0100

    added proper padding to make it look nicer

--- src/components/tours/Tours.jsx
+++ src/components/tours/Tours.jsx
@@ -29,6 +29,54 @@ class Tours extends Component {
               <div className="tours-dates-tickets-button">TICKETS</div>
             </div>
           </div>
+          <div className="tours-dates">
+            <div className="tours-dates-date">
+              <h3 className="tours-dates-date-month">APR</h3>
+              <h3 className="tours-dates-date-day">30</h3>
+              <h3 className="tours-dates-date-year">2018</h3>
+            </div>
+            <div className="tours-dates-venue">
+              <h3 className="tours-dates-venue-city">
+                <p>Stockholm, Sweden</p>
+              </h3>
+              <h3 className="tours-dates-venue-name">Ericsson Globe</h3>
+            </div>
+            <div className="tours-dates-tickets">
+              <div className="tours-dates-tickets-button">TICKETS</div>
+            </div>
+          </div>
+          <div className="tours-dates">
+            <div className="tours-dates-date">
+              <h3 className="tours-dates-date-month">APR</h3>
+              <h3 className="tours-dates-date-day">30</h3>
+              <h3 className="tours-dates-date-year">2018</h3>
+            </div>
+            <div className="tours-dates-venue">
+              <h3 className="tours-dates-venue-city">
+                <p>Stockholm, Sweden</p>
+              </h3>
+              <h3 className="tours-dates-venue-name">Ericsson Globe</h3>
+            </div>
+            <div className="tours-dates-tickets">
+              <div className="tours-dates-tickets-button">TICKETS</div>
+            </div>
+          </div>
+          <div className="tours-dates">
+            <div className="tours-dates-date">
+              <h3 className="tours-dates-date-month">APR</h3>
+              <h3 className="tours-dates-date-day">30</h3>
+              <h3 className="tours-dates-date-year">2018</h3>
+            </div>
+            <div className="tours-dates-venue">
+              <h3 className="tours-dates-venue-city">
+                <p>Stockholm, Sweden</p>
+              </h3>
+              <h3 className="tours-dates-venue-name">Ericsson Globe</h3>
+            </div>
+            <div className="tours-dates-tickets">
+              <div className="tours-dates-tickets-button">TICKETS</div>
+            </div>
+          </div>
         </div>
         <Footer />
       </div>
